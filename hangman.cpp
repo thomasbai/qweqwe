8a28bca1e58389b6da6835b180acbd915ef5c8d1
thomasbai <52986286+thomasbai@users.noreply.github.com>
2019-07-23 08:33:10 +0800

    Add files via upload

---
+++ hangman.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<conio.h> 
+#include<string.h>
+15041968
+void main()
+{
+int i,j,c,count=0,ans=0,flag=0£¬*prt;
+char a[1][6]={'d','o',godo};
+
+char b[10],alpha;
+char d='_';
+clrscr(); 
+c=strlen(&a[0][0]);
+//printf("\n\t\t**************\n\n\t\t\t");
+printf("\n\n\t\t\t**HANGMAN**\n");
+		printf("\n\t\t\t**************\t\t\t");
+				printf("\n\t\t\t..............\n\n\t\t\t")	
+for(j=0;j<c;j++)
+		{printf("%c",d);
+		b[j]=d;}
+		//printf("\n\n\t\t**************");
+		printf("\n\n\t\t\t.............\t\t\t");
+			printf("\n\n\t\t\t**************");
+//for(j=0;j<c;j++)printf("\n%c",b[j]);//{ ptr=&b[j];
+	// printf("\n%c",*ptr;}
+while(count<6)//||(ans<c))
+{
+flag=0;
+		printf("\n\n\n\n\n\n\t enter a char");
+				alpha=getche();
+			//	printf("%c",*ptr);}
+				for(i=o;i<c;i++)
+				{
+					if(alpha==a[0][i])
+				{	b[i]=a[0][i];
+flag=1;
+		ans++;}
+		//else if(b[i]!=d) b[i]=a[0][i];
+		//else b[i]=d;
+	//	printf("\n u r correct");}
+	//	if(ans==c)goto man;}
+		//printf("\n ans=%d",ans);}
+		
+		//printf("\n count=%d",count);
+		
+    }
+clrscr();
+printf("\n\n\t\t\t**HANGMAN**\n");
+printf("\n\t\t\t**************\t\t\t");
+printf("\n\n\t\t\t.............\t\t\t");
+for(i=0;i<c;i++)
+printf("%c",b[i])
+printf("\n\n\t\t\t..............\t\t\t");
+printf("\n\n\t\t\t**************");
+	output[i]='_';
+}
+	for(i=0;i,c,i++)
+	
+}*/
+getch();
+
+}
+
+	
